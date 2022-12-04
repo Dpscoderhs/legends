9791aedc5ab075fc4d240d04ed61e00bc5795e5c
Dpscoderhs <104405006+Dpscoderhs@users.noreply.github.com>
2022-12-04 17:36:54 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,13 +11,15 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 640, 480);
+   fill(0, 200, 0)
     circle(30, 30, 70);
-    rect(60, 25, 515, 20);
     circle(605, 30, 70);
-
     circle(30, 470, 70);
-    rect(60, 470, 515, 20);
     circle(605, 470, 70);
+    circle(30, 470, 70);
+    fill(200, 0, 0)
+    rect(60, 25, 515, 20);
+    rect(60, 470, 515, 20);
     rect(20, 50, 20, 400);
     rect(60, 470, 515, 20);
     rect(590, 50, 20, 400);
